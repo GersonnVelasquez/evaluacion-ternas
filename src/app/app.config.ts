0bb2a7bbd975429c41199befd969638f41dcfed8
Gersonn Velasquez <yersonn.velasquez@gmail.com>
2025-04-30 13:34:29 -0600

    agregando firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "evaluacion-ternas", appId: "1:391646671824:web:6dce9cf604d8a973e0bfd5", storageBucket: "evaluacion-ternas.firebasestorage.app", apiKey: "AIzaSyBoAATZVgLnCHKlqtaoJxcmJPdHEOJ2z98", authDomain: "evaluacion-ternas.firebaseapp.com", messagingSenderId: "391646671824" })), provideFirestore(() => getFirestore())]
 };
