1cae486d498700eaec73585f6290195270bfff72
Gersonn Velasquez <31450496+GersonnVelasquez@users.noreply.github.com>
2025-05-09 09:38:37 -0600

    Avance grupo 1 (#1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    
+    {
+        path: '',
+        redirectTo: 'participantes',
+        pathMatch: 'full'
+
+
+    },
+    {
+        path: 'participantes',
+        loadComponent: () => import('./features/participantes/components/participantes/participantes.component').then(m => m.ParticipantesComponent)
+    }
+];
---
+++ src/app/features/participantes/models/participante.model.ts
@@ -0,0 +1,9 @@
+export interface Participante {
+nombreCompleto :string
+numeroCuenta:number | null
+nombreProyecto:string
+tipoProyecto : string
+enlaceDocumentoTesis:string
+notaParcialFase2:number | null
+nombreCatedraticoFase2: string
+}
